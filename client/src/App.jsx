1d76dc20705615d14ceef92983240a3ea6f63fe9
jubaer69 <silentkiller@gmail.com>
2024-12-28 01:43:18 +0600

    third commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 import { Outlet } from 'react-router-dom'
 import Header from './components/Header'
-import Cart from './components/cart'
+import Cart from './components/Cart'
+
+// import Cart from './components/cart'
 
 function App() {
   
